d26bf211986221e42f12cefb953a3d80608c0c45
moscaluionutmihai <32124907+moscaluionutmihai@users.noreply.github.com>
2018-11-19 14:46:44 +0200

    Add files via upload

---
+++ sql/@5.99_create_archive_pkg.sql
@@ -0,0 +1,69 @@
+[1;63r[27m[24m[0m[H[J[63;1H"CANAL_PLUS@bkupdb" [New File][2;1H[1m~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~[1;1H[63;1H-- INSERT --[0m[63;13H[K[1;1Hrning: Package Body created with compilation errors.[2;1H[K[3;1HCANAL_PLUS@bkupdb > show err
+Errors for PACKAGE BODY CANAL_PLUS.ARCHIVE_PKG:[5;1H[K[6;1HLINE/COL ERROR
+-------- -----------------------------------------------------------------
+113/10   PL/SQL: SQL Statement ignored
+113/27   PL/SQL: ORA-01747: invalid user.table.column, table.column, or
+ [7C column specification[11;1H[K[12;1H[K[12;1H[13;1H[K[13;1H[14;1H[K[14;1H[15;1H[K[15;1H[16;1H[K[16;1H[17;1H[K[17;1H[18;1H[K[18;1H[19;1H[K[19;1H[20;1H[K[20;1H[21;1H[K[21;1H[63;1H[K[21;1H[63;1HType  :quit<Enter>  to exit Vim[21;1H[63;1H[K[63;1H:q[7mE37: No write since last change (add ! to override)[21;1H[63;1H[27mVim: Error reading input, exiting...
+Vim: preserving files...
+Vim: Finished.
+[63;1H[63;1H[K[63;1H
